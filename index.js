859ab7e00b58954457572f8741183afb7bdd4be5
Muhammad Umar <muhammadumar.tudortmund@gmail.com>
2024-01-21 15:49:29 +0500

    Go through each div of nodelist and add event handlers for hovering affect

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let gridSize = prompt('Enter n * n grid size','16');
 
-const gridLoop = gridSize * gridSize; //For square grid of n*n
-const gapPixels = gridSize - 1; // so that gap-pixels gets subtracted from total
-const gridFlexPixels = (960 - gapPixels) / gridSize; //formula to get n*n grid
-const container = document.querySelector('#container');
+//For loop of square grid of n*n
+const gridLoop = gridSize * gridSize; 
+// so that gap-pixels gets subtracted from total
+const gapPixels = gridSize - 1; 
+//formula to get n*n grid
+const gridFlexPixels = (960 - gapPixels) / gridSize; 
 
+const container = document.querySelector('#container');
 // To get n-square divs for grid
 for(let i = 1; i <= gridLoop; i++){  
         const div = document.createElement('div');
@@ -17,10 +20,18 @@ for(let i = 1; i <= gridLoop; i++){
 var gridFlexItems = document.querySelectorAll('.gridElement'); 
 
 //go through each noselist(array-like) div and assign flex-basis
-//and height using formula abve
+//and height using formula above. Also add eventlisteners
+//for hovering effect
 gridFlexItems.forEach((item) => {
     item.style.flexBasis = gridFlexPixels + 'px';
     item.style.height = gridFlexPixels + 'px';
+
+    item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = '#ff000080';
+    });
+    item.addEventListener("mouseout", () => {
+        item.style.backgroundColor = '#7b3f00';
+    });
 })
 
 
