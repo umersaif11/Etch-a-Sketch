7ed0bcc154ab04a00691f57726e28ecfe47968ab
Muhammad Umar <muhammadumar.tudortmund@gmail.com>
2024-01-23 20:42:45 +0500

    Use inner.html to clear existing grid

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-
+//function to put in click-event-listener of button
 function grid(){
+    //container-content clears out so that existing
+    //grid is removed
+    document.getElementById("container").innerHTML = ''; 
     let gridSize = prompt('Enter n * n grid size','16');
-    const container = document.querySelector('#container');
 
     //For loop of square grid of n*n
     const gridLoop = gridSize * gridSize; 
-    // so that gap-pixels gets subtracted from total
+    //so that gap-pixels gets subtracted from total
     const gapPixels = gridSize - 1; 
-    //formula to get n*n grid pixels
+    //formula to get pixels of single box
+    //of n*n grid pixels
     const gridFlexPixels = (960 - gapPixels) / gridSize;
-
+    
+    const container = document.querySelector('#container');
     // To get n-square divs for grid
     for(let i = 1; i <= gridLoop; i++){  
            const div = document.createElement('div');
@@ -17,25 +21,27 @@ function grid(){
            div.classList.add('gridElement');
            container.appendChild(div);
     }
-    
+
    //to get nodelist of all divs
    let gridFlexItems = document.querySelectorAll('.gridElement');
 
-   //go through each noselist(array-like) div and assign flex-basis
-   //and height using formula above. Also add eventlisteners
-   //for hovering effect
+   //go through each noselist(array-like) div and assign 
+   //flex-basis(width) and height using formula above. 
+   //Also add eventlisteners for hovering effect
    gridFlexItems.forEach((item) => {
        item.style.flexBasis = gridFlexPixels + 'px';
        item.style.height = gridFlexPixels + 'px';
 
+       //when mouse cursor goes into box
        item.addEventListener("mouseover", () => {
            item.style.backgroundColor = '#ff000080';
         });
+       //when mouse cursor goes out of box
+       //to retain its original color 
        item.addEventListener("mouseout", () => {
            item.style.backgroundColor = '#7b3f00';
         });
 })
-
 }  
 
 //event listener for button to click and get grid
